fde0a0f7aa040e4b5678df8ab8cedd951d458cc8
dominicspina <43656595+dominicspina@users.noreply.github.com>
2018-10-11 08:46:45 -0600

    Add files via upload
    This is the program to operate the arm on 10736.

---
+++ arm-DominicSpina.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Arm;
+
+/*
+-----ARM CONTROL-----
+
+Comments below this line----------------------------------------------------------------------------
+
+Notes:
+Arm shape will change, still determining to use spring-loaded servo mount and guide rail or 2 free
+mounted servos.
+ */
+
+public class DominicSpina {
+}
